75e305b98137cd1fb8ab411a4c0818e9ce8454d6
MaBing <bma@hpe.com>
2023-11-07 15:21:30 +0800

    update

--- webflux-demo/src/main/java/com/linyi/webflux/controller/LogFilter.java
+++ webflux-demo/src/main/java/com/linyi/webflux/controller/LogFilter.java
@@ -15,11 +15,13 @@ public class LogFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // TODO pre-hanlder
         long startTime = System.currentTimeMillis();
         String path = exchange.getRequest().getURI().getPath();
         log.info("Serving '{}'", path);
 
         return chain.filter(exchange).doAfterTerminate(() -> {
+                    // TODO after-hanlder
                     exchange.getResponse().getHeaders().entrySet().forEach(e ->
                             log.info("Response header '{}': {}", e.getKey(), e.getValue()));
 
